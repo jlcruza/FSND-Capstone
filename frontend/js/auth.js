1f5ab3d8907fb6fcb4c880823243e482d0bab040
Jorge Cruz <jlcruz7125@gmail.com>
2020-05-14 22:00:55 -0400

    Account login

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,17 +1,75 @@
-const authenticaotr = (()=>{
-    let auth0 = null;
+const authenticaotr = (() => {
 
-    // const fetchAuthConfig = () => fetch("/auth_config.json");
+  // The Auth0 client, initialized in configureClient()
+  let auth0 = null;
+  const targetUrl = "https://casting-agency-capstone.auth0.com/authorize?audience=executive&response_type=token&client_id=o8j0f3DEN9v3URxAXn1o65vJorxZNLJ1&redirect_uri=https://jlcruza.github.io/FSND-Capstone/frontend/"
 
-    const configureClient = async () => {
-        // const response = await fetchAuthConfig();
-        // const config = await response.json();
-      
-        auth0 = await createAuth0Client({
-          domain: "casting-agency-capstone.auth0.com",
-          client_id: "o8j0f3DEN9v3URxAXn1o65vJorxZNLJ1"
-        });
+  /**
+* Starts the authentication flow
+*/
+  const login = async () => {
+    try {
+      console.log("Logging in", targetUrl);
+
+      const options = {
+        redirect_uri: window.location.origin
       };
 
-    return {auth0, configureClient}
+      if (targetUrl) {
+        options.appState = { targetUrl };
+      }
+
+      await auth0.loginWithRedirect(options);
+    } catch (err) {
+      console.log("Log in failed", err);
+    }
+  };
+
+
+  /**
+   * Executes the logout flow
+   */
+  const logout = () => {
+    try {
+      console.log("Logging out");
+      auth0.logout({
+        returnTo: window.location.origin
+      });
+    } catch (err) {
+      console.log("Log out failed", err);
+    }
+  };
+
+
+  /**
+   * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+   * is prompted to log in
+   * @param {*} fn The function to execute if the user is logged in
+   */
+  const requireAuth = async (fn) => {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+      return fn();
+    }
+
+    return login(targetUrl);
+  };
+
+
+  const configureClient = async () => {
+    // const response = await fetchAuthConfig();
+    // const config = await response.json();
+
+    auth0 = await createAuth0Client({
+      domain: "casting-agency-capstone.auth0.com",
+      client_id: "o8j0f3DEN9v3URxAXn1o65vJorxZNLJ1"
+    });
+  };
+
+  const getToken = () => {
+    return auth0;
+  }
+
+  return { configureClient, getToken, login, logout, requireAuth }
 })()
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,11 +1,6 @@
 (function () {
 
-    const updateUI = async () => {
-        await authenticaotr.configureClient()
-        const isAuthenticated = await authenticaotr.auth0.isAuthenticated();
-
-        console.log("isAuthenticated", isAuthenticated);
-    };
-
-    updateUI()
+    document.getElementById('btn-login')
+    .addEventListener('click', authenticaotr.login())
+    
 })()
